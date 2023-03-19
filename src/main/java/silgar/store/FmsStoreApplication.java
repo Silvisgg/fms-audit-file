4d47167accb0caaa00ea7b6fc663dae5fa721162
silvis_sil <silvis.garcia@gmail.com>
2023-03-19 21:09:16 +0100

    first commit

---
+++ src/main/java/silgar/store/FmsStoreApplication.java
@@ -0,0 +1,13 @@
+package silgar.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmsStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmsStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/silgar/store/FmsStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package silgar.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmsStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
