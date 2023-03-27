26b3a116f817a845746cbac94299d026f85049fe
silvis_sil <silvis.garcia@gmail.com>
2023-03-27 22:02:11 +0200

    Refactor

--- src/main/java/silgar/store/minio/MinioConfiguration.java
+++ src/main/java/silgar/store/minio/MinioConfiguration.java
@@ -18,82 +18,82 @@ import java.net.Proxy;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
-    @Configuration
-    public class MinioConfiguration {
+@Configuration
+public class MinioConfiguration {
 
-        private static final Logger LOGGER = LoggerFactory.getLogger(MinioConfiguration.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(MinioConfiguration.class);
 
-        @Autowired
-        private MinioConfigurationProperties minioConfigurationProperties;
+    @Autowired
+    private MinioConfigurationProperties minioConfigurationProperties;
 
-        @Bean
-        public MinioClient minioClient() throws IOException, InvalidKeyException, NoSuchAlgorithmException, MinioException {
+    @Bean
+    public MinioClient minioClient() throws IOException, InvalidKeyException, NoSuchAlgorithmException, MinioException {
 
-            MinioClient minioClient;
-            if (!configuredProxy()) {
-                minioClient = MinioClient.builder()
-                        .endpoint(minioConfigurationProperties.getUrl())
-                        .credentials(minioConfigurationProperties.getAccessKey(), minioConfigurationProperties.getSecretKey())
-                        .build();
-            } else {
-                minioClient = MinioClient.builder()
-                        .endpoint(minioConfigurationProperties.getUrl())
-                        .credentials(minioConfigurationProperties.getAccessKey(), minioConfigurationProperties.getSecretKey())
-                        .httpClient(client())
-                        .build();
-            }
-            minioClient.setTimeout(
-                    minioConfigurationProperties.getConnectTimeout().toMillis(),
-                    minioConfigurationProperties.getWriteTimeout().toMillis(),
-                    minioConfigurationProperties.getReadTimeout().toMillis()
-            );
-
-            if (minioConfigurationProperties.isCheckBucket()) {
-                try {
-                    LOGGER.debug("Checking if bucket {} exists", minioConfigurationProperties.getBucket());
-                    BucketExistsArgs existsArgs = BucketExistsArgs.builder()
-                            .bucket(minioConfigurationProperties.getBucket())
-                            .build();
-                    boolean b = minioClient.bucketExists(existsArgs);
-                    if (!b) {
-                        if (minioConfigurationProperties.isCreateBucket()) {
-                            try {
-                                MakeBucketArgs makeBucketArgs = MakeBucketArgs.builder()
-                                        .bucket(minioConfigurationProperties.getBucket())
-                                        .build();
-                                minioClient.makeBucket(makeBucketArgs);
-                            } catch (Exception e) {
-                                throw new MinioException("Cannot create bucket");
-                            }
-                        } else {
-                            throw new IllegalStateException("Bucket does not exist: " + minioConfigurationProperties.getBucket());
-                        }
-                    }
-                } catch (Exception e) {
-                    LOGGER.error("Error while checking bucket", e);
-                    throw e;
-                }
-            }
-
-            return minioClient;
-        }
-
-        private boolean configuredProxy() {
-            String httpHost = System.getProperty("http.proxyHost");
-            String httpPort = System.getProperty("http.proxyPort");
-            return httpHost != null && httpPort != null;
-        }
-
-        private OkHttpClient client() {
-            String httpHost = System.getProperty("http.proxyHost");
-            String httpPort = System.getProperty("http.proxyPort");
-
-            OkHttpClient.Builder builder = new OkHttpClient.Builder();
-            if (httpHost != null)
-                builder.proxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress(httpHost, Integer.parseInt(httpPort))));
-            return builder
+        MinioClient minioClient;
+        if (!configuredProxy()) {
+            minioClient = MinioClient.builder()
+                    .endpoint(minioConfigurationProperties.getUrl())
+                    .credentials(minioConfigurationProperties.getAccessKey(), minioConfigurationProperties.getSecretKey())
+                    .build();
+        } else {
+            minioClient = MinioClient.builder()
+                    .endpoint(minioConfigurationProperties.getUrl())
+                    .credentials(minioConfigurationProperties.getAccessKey(), minioConfigurationProperties.getSecretKey())
+                    .httpClient(client())
                     .build();
         }
+        minioClient.setTimeout(
+                minioConfigurationProperties.getConnectTimeout().toMillis(),
+                minioConfigurationProperties.getWriteTimeout().toMillis(),
+                minioConfigurationProperties.getReadTimeout().toMillis()
+        );
 
+        if (minioConfigurationProperties.isCheckBucket()) {
+            try {
+                LOGGER.debug("Checking if bucket {} exists", minioConfigurationProperties.getBucket());
+                BucketExistsArgs existsArgs = BucketExistsArgs.builder()
+                        .bucket(minioConfigurationProperties.getBucket())
+                        .build();
+                boolean b = minioClient.bucketExists(existsArgs);
+                if (!b) {
+                    if (minioConfigurationProperties.isCreateBucket()) {
+                        try {
+                            MakeBucketArgs makeBucketArgs = MakeBucketArgs.builder()
+                                    .bucket(minioConfigurationProperties.getBucket())
+                                    .build();
+                            minioClient.makeBucket(makeBucketArgs);
+                        } catch (Exception e) {
+                            throw new MinioException("Cannot create bucket");
+                        }
+                    } else {
+                        throw new IllegalStateException("Bucket does not exist: " + minioConfigurationProperties.getBucket());
+                    }
+                }
+            } catch (Exception e) {
+                LOGGER.error("Error while checking bucket", e);
+                throw e;
+            }
+        }
+
+        return minioClient;
     }
+
+    private boolean configuredProxy() {
+        String httpHost = System.getProperty("http.proxyHost");
+        String httpPort = System.getProperty("http.proxyPort");
+        return httpHost != null && httpPort != null;
+    }
+
+    private OkHttpClient client() {
+        String httpHost = System.getProperty("http.proxyHost");
+        String httpPort = System.getProperty("http.proxyPort");
+
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        if (httpHost != null)
+            builder.proxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress(httpHost, Integer.parseInt(httpPort))));
+        return builder
+                .build();
+    }
+
+}
 
