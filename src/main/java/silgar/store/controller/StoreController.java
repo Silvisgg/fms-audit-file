a5393ad245e43b0284f916579cb55c5f8683bea9
silvis_sil <silvis.garcia@gmail.com>
2023-05-11 20:35:44 +0200

    [FMS Store - Reactive Response-3]

--- src/main/java/silgar/store/controller/StoreController.java
+++ src/main/java/silgar/store/controller/StoreController.java
@@ -1,19 +1,23 @@
 package silgar.store.controller;
 
 import io.minio.errors.MinioException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import reactor.core.publisher.Mono;
+import silgar.store.exception.ErrorMessage;
 import silgar.store.service.IStoreService;
 
-import java.io.IOException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 
+@Slf4j
 @RestController
 public class StoreController {
 
@@ -27,17 +31,31 @@ public class StoreController {
     /*
     Store a file to the Storage
      */
-    @PostMapping(path = "/store-s3")
+    @PostMapping(path = "/file/store-s3", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     @ResponseBody
-    public ResponseEntity<String> handleFileStore(@RequestParam("file") MultipartFile file) {
+    public Mono<ResponseEntity> handleSingleFileStore(@RequestParam("file") MultipartFile inbound){
 
-        iStoreService.store(file);
+        log.info("Entering handleFileStore. ");
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Custom-Header", "StoreController");
+        return Mono.just(inbound)
+                .doOnNext(iStoreService::store)//actua como una sonda y reacciona. envia y recibe info pero no la transforma
+                .map( file -> {  // transformacion plana de un obj a otro obj - flatmap a varios objetos
+                    log.info("Entering map file");
 
-        return ResponseEntity.ok().headers(headers).body("You successfully stored " + file.getOriginalFilename() + "!");
+                    HttpHeaders headers = new HttpHeaders();
+                    headers.add("Custom-Header", "StoreController");
+                    ResponseEntity responseEntity = ResponseEntity.ok()
+                            .headers(headers)
+                            .body("File " + file.getOriginalFilename() + " stored!");
+                        return responseEntity;
+                })
+                .onErrorResume(exc -> {
+                    log.info("Entering onErrorResume. ");
+                    return throwExc(exc);
+
+                });//convertir apiExcephandler a servicio
+                         // en caso de error no se detiene el flujo
     }
 
 
@@ -52,4 +70,38 @@ public class StoreController {
     }
 
 
+    /*
+Check if the application is running
+ */
+    @GetMapping(path = "/healthmono/{cadena}")
+    @ResponseBody
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<ResponseEntity> statusMono(@PathVariable("cadena") String cadena) {
+
+        return Mono.just(cadena)
+                .map( palabra -> {
+                    HttpHeaders headers = new HttpHeaders();
+                    headers.add("Custom-Header", "HealthController");
+                    ResponseEntity responseEntity = ResponseEntity.ok()
+                            .headers(headers)
+                            .body("Store Application is running... Mono controller. Palabra: " + palabra);
+                    return responseEntity;
+                })
+                .onErrorResume(exc -> throwExc(exc));
+    }
+
+
+    private Mono<ResponseEntity> throwExc (Throwable exc){
+    HttpHeaders headers = new HttpHeaders();
+                            headers.add("Custom-Header", "Controller advice");
+
+    ErrorMessage errorMessage = new ErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            new Date(),
+            exc.getMessage(),
+            exc.getStackTrace().toString(),
+            "");
+log.info("ERRORRR: "+errorMessage.getMessage());
+    return Mono.just(new ResponseEntity (errorMessage,headers,HttpStatus.INTERNAL_SERVER_ERROR));
+
+    }
 }
